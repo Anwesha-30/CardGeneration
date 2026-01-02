ac8e3171f3216837c02de990b3b0c81cb666c5e0
Anwesha-30 <anwesha.samaddar.cse.2024@tint.edu.in>
2026-01-02 23:44:58 +0530

    Add the changes

--- src/components/Section1/LeftContent.jsx
+++ src/components/Section1/LeftContent.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import { MoveUpRight } from "lucide-react";
 const LeftContent = () => {
   return (
-    <div className='h-full w-1/4 bg-blue-500'>
-      
+    <div className='h-full flex w-1/4 bg-blue-300 '>
+      <div>
+        <h3 className='mb-5 text-4xl font-bold'>Prospective <br/><span>Customer </span><br/>segmentation</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis maiores, praesentium inventore nesciunt incidunt error sint odit reiciendis cumque delectus.</p>
+      </div>
+      <div>
+        <MoveUpRight className="w-16 h-16 text-black transition-transform hover:translate-x-1 hover:-translate-y-1" />
+
+      </div>
     </div>
   )
 }
--- src/components/Section1/RightContent.jsx
+++ src/components/Section1/RightContent.jsx
@@ -4,9 +4,7 @@ const RightContent = () => {
   return (
     <div className='h-full w-3/4 bg-blue-500'>
       <div></div>
-      <div>
-          <MoveUpRight />  
-      </div>
+      
        </div>
   )
 }
